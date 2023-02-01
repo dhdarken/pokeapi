09b6a639426bf88311f951b682da2f66937f9cf4
julian <juliansanvill@hotmail.com>
2023-02-01 16:42:22 -0500

    pokeapi

---
+++ index.js
@@ -0,0 +1,43 @@
+const pokeList= document.getElementById("pokelist");
+
+
+(()=>{
+    const pokemonshtml =document.getElementById("pokelist")
+fetch("https://pokeapi.co/api/v2/pokemon?offset=10&limit=200")
+.then((
+    resolved
+)=>
+resolved.json()
+
+)
+.then((resolved)=>{
+    console.log(resolved)
+    let pokemonsli="";
+    let poke=resolved.results;
+    for(let i=0;i<3900;i++){
+    const ID=poke[i].url.split("/")[6];
+    console.log(ID)
+    const pokename= poke[i].name;
+    console.log(pokename);
+    pokemonsli=pokemonsli+`
+    <li>
+        <div>
+            <div id="triangulo">
+                <span id="numero">${ID}</span>
+            </div>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${ID}.png" alt="foto de el pokemon ${pokename} ">
+            <span id="names">${pokename}</span>
+        </div>
+    </li>
+   `
+    pokemonshtml.innerHTML=pokemonsli    
+}
+
+})
+.catch((error)=>{
+console.log(error)
+})
+
+
+}) ();
+    
